2f20624b83674eeddd2e1dcb1ceb655ef760027e
Dukq0418 <842749680@qq.com>
2020-01-30 20:14:07 +0800

    书店页面

--- src/views/book/Add_book.vue
+++ src/views/book/Add_book.vue
@@ -13,29 +13,29 @@
       <el-input type="textarea" v-model="form.descr"></el-input>
     </el-form-item>
     <el-form-item label="书籍分类">
-      <el-select v-model="form.class.name" @change="selectChange" clearable placeholder="请选择">
+      <el-select v-model="form.bookClass" @change="selectChange" clearable placeholder="请选择">
         <el-option
-          v-for="item in selectList"
+          v-for="item in bookClassList"
           :key="item.id"
-          :label="item.id"
-          :value="item.name">
+          :label="item.name"
+          :value="item.id">
         </el-option>
       </el-select>
     </el-form-item>
-    <el-form-item align="center">
-      <el-upload
-        class="upload-demo"
-        action="https://jsonplaceholder.typicode.com/posts/"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :before-remove="beforeRemove"
-        multiple
-        :limit="3"
-        :on-exceed="handleExceed"
-        :file-list="fileList">
-        <el-button size="small" type="primary">点击上传</el-button>
-      </el-upload>
-    </el-form-item>
+<!--    <el-form-item align="center">-->
+<!--      <el-upload-->
+<!--        class="upload-demo"-->
+<!--        action="https://jsonplaceholder.typicode.com/posts/"-->
+<!--        :on-preview="handlePreview"-->
+<!--        :on-remove="handleRemove"-->
+<!--        :before-remove="beforeRemove"-->
+<!--        multiple-->
+<!--        :limit="3"-->
+<!--        :on-exceed="handleExceed"-->
+<!--        :file-list="fileList">-->
+<!--        <el-button size="small" type="primary">点击上传</el-button>-->
+<!--      </el-upload>-->
+<!--    </el-form-item>-->
     <el-form-item>
       <el-button type="primary" @click="onSubmit">立即创建</el-button>
       <el-button>取消</el-button>
@@ -53,17 +53,30 @@
     export default {
         data() {
             return {
+                bookClassList: [],
                 form: {
                     bookname: '',
+                    bookClass:{
+                      name: '', descr: ''
+                    },
                     author: '',
                     price: 0,
                     classid: 0,
                     descr: ''
                 },
-                fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+                // fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
             };
         },
+        created(){
+          this.getBookClassList()
+        },
         methods: {
+            //获取所有商品分类数据
+            async getBookClassList(){
+             const {data: res} = await this.$http.get('/bookClass/queryAll')
+                this.bookClassList = res;
+             console.log(res)
+            },
             handleRemove(file, fileList) {
                 console.log(file, fileList);
             },
@@ -74,30 +87,33 @@
                 this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
             },
             beforeRemove(file, fileList) {
-                return this.$confirm(`确定移除 ${ file.name }？`);
+                return this.$confirm('确定移除 ${ file.name }？');
+            },
+            onSubmit(){
+
             },
             selectChange(val){
-                var obj = {};
-                obj = this.selectList.find(function(item){
-                    return item.name === val;
-                })
-                this.editForm.id = obj.id;
-                this.edit.name = obj.name;
+                // var obj = {};
+                // obj = this.selectList.find(function(item){
+                //     return item.name === val;
+                // })
+                // this.editForm.id = obj.id;
+                // this.editForm.name = obj.name;
             }
         },
         computed: {
             selectList(){
-                let obj = [];
-                this.axios.get("/class/queryAll")
-                    .then(resp => {
-                        resp.data.forEach((item, index) => {
-                            obj.push({
-                                id: item.id,
-                                name: item.name
-                            });
-                        });
-                    });
-                return obj;
+                // let obj = [];
+                // this.axios.get("/class/queryAll")
+                //     .then(resp => {
+                //         resp.data.forEach((item, index) => {
+                //             obj.push({
+                //                 id: item.id,
+                //                 name: item.name
+                //             });
+                //         });
+                //     });
+                // return obj;
             },
         },
     }
--- src/views/book/List.vue
+++ src/views/book/List.vue
@@ -3,7 +3,7 @@
     <el-main>
       <!--  根据名字查找  -->
       <el-table
-        :data="books"
+        :data="bookList"
         style="width: 100%">
         <el-table-column
           label="序号"
@@ -62,9 +62,13 @@
     </el-main>
     <el-footer>
       <el-pagination
-        background
-        layout="prev, pager, next"
-        :total="100">
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pageNum"
+        :page-sizes="[1, 2, 5]"
+        :page-size="queryInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
       </el-pagination>
     </el-footer>
 <!--    修改用户的对话框  -->
@@ -88,7 +92,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="editDialogVisible  = false">取 消</el-button>
-    <el-button type="primary" @click="editDialogVisible = false">确 定</el-button>
+    <el-button type="primary" @click="editBookInfo">确 定</el-button>
        </span>
     </el-dialog>
   </el-container>
@@ -103,7 +107,16 @@
     export default {
         data() {
             return {
-                books:[],
+                //获取用户列表的参数对象
+                queryInfo:{
+                  query:'',
+                    //当前页数
+                  pageNum: 1,
+                    //当前每页显示多少条
+                  pageSize: 2
+                },
+                bookList:[],
+                total: 0,
                 //控制修改用户对话框的显示与隐藏
                 editDialogVisible: false,
                 //查询到的用户信息对象
@@ -114,41 +127,58 @@
                         {required: true, message: '请输入作者信息',trigger: 'blur'},
                     ],
                     price: [
-                        {required: true, message: '请输入作者信息',trigger: 'blur'},
+                        {required: true, message: '请输入电话号码',trigger: 'blur'},
                     ]
                 }
             }
         },
-        mounted(){
-            this.axios.get('/book/queryAll')
-                .then(resp => {
-                    this.books = resp.data;
-                })
+        created(){
+            this.getBookList()
         },
-
         methods: {
-            handleDelete(id){
-                this.$confirm('此操作将永久删除该条信息, 是否继续?','提示',{
+            async getBookList(){
+              const { data: res } = await this.$http.get('/book/queryAll', {
+                  params:this.queryInfo
+              })
+                console.log(res);
+                this.bookList = res.list,
+                this.total = res.total
+
+            },
+            //监听 pageSize 改变的事件
+            handleSizeChange(newSize){
+                console.log(newSize)
+                this.queryInfo.pageSize = newSize
+                this.getBookList()
+            },
+            //监听页码值改变的事件
+            handleCurrentChange(newPage){
+                console.log(newPage)
+                this.queryInfo.pageNum = newPage
+                this.getBookList()
+            },
+
+            async handleDelete(id){
+                const confirmResult = await this.$confirm(
+                    '此操作将永久删除该条信息, 是否继续?',
+                    '提示',
+                    {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
                     type: 'warning'
-                }).then(()=>{
-                    this.axios.get('/book/delete?id=' + id)
-                        .then(resp => {
-                            if (resp.data == 'success') {
-                                this.$router.push("/book/list")
-                            }else {
-                                this.$notify({
-                                    title: '提示',
-                                    message: '删除失败，请联系管理员'
-                                });
-                            }
-                        })
-                    this.$message({
-                        type: "success",
-                        message: '删除成功'
-                    })
-                })
+                    }
+                ).catch(err => err)
+                if(confirmResult !== 'confirm'){
+                    return this.$message.info('已取消删除')
+                }
+
+                const {data: res} = await this.$http.get('/book/delete?id=' + id)
+                if(res == 'success'){
+                    this.$message.success('删除成功！')
+                    this.getBookList()
+                }else{
+                    return this.$message.error('删除用户成功！')
+                }
             },
             async showEditDialog(id){
                 // console.log(id)
@@ -159,6 +189,31 @@
                         }
                     })
                 this.editDialogVisible = true;
+            },
+            //修改用户信息并提交
+            editBookInfo(){
+                this.$refs.editFormRef.validate(valid=>{
+                    console.log(valid)
+                    if(!valid) return
+                //发起修改用户信息的数据请求
+                    this.$http.get('/book/edit?id=' + this.editForm.id
+                        +'&author=' + this.editForm.author
+                        +'&price=' + this.editForm.price
+                        +'&descr=' + this.editForm.descr)
+                        .then(resp => {
+                            if(resp.data == "success"){
+                               // 1.关闭对话框
+                                this.editDialogVisible = false
+                               // 2.刷新数据列表
+                                this.getBookList()
+                               // 3.提示修改成功
+                                this.$message.success('更新用户成功！')
+                            }else{
+                                return this.$message.error('更新用户信息失败！')
+                            }
+                        })
+                })
+
             }
         }
     }
